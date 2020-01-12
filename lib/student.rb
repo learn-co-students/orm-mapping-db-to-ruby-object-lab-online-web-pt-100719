e98fc4d5ded6a39bf628d69c03ba90ce322dc1e1
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-01-12 22:11:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,7 @@ class Student
     end
   end
 
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
@@ -68,25 +69,42 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.students_below_12th_grade
-    sql = <<-SQL
-    SELECT * 
-    FROM students 
-    WHERE grade < 12 
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end
-  end
+  
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL 
     SELECT * 
     FROM students 
     WHERE grade=10 
-    ORDER BY students.id LIMIT ?
+    LIMIT ?
     SQL
     DB[:conn].execute(sql, x)
   end
+
+   def self.students_below_12th_grade
+    sql = "SELECT * 
+    FROM students 
+    WHERE grade < 12"
+    DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
+    end
+  end
   
+   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, x)
+  end
+
 end
